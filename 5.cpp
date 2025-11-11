70091e3f77ea32b119329080b89f4ed144cd984b
sarthak-70 <cu25720001@coeruniversity.ac.in>
2025-11-11 09:48:56 +0530

    Count even and odd numbers

---
+++ 5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[10], even=0, odd=0;
+    cout << "Enter 10 numbers:\n";
+    for(int i=0; i<10; i++){
+        cin >> arr[i];
+        if(arr[i] % 2 == 0) even++;
+        else odd++;
+    }
+    cout << "Even: " << even << ", Odd: " << odd;
+    return 0;
+}
